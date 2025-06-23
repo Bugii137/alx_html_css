96da37920d8e6be86a6426e2b83edb953b090c4e
Mark Kabugi <kabug222@gmail.com>
2025-06-23 10:06:32 +0300

    section 8

---
+++ headphones/8-script.js
@@ -0,0 +1,10 @@
+// 8-script.js
+
+document.addEventListener('DOMContentLoaded', function () {
+  const hamburger = document.querySelector('.hamburger');
+  const menu = document.querySelector('.menu');
+
+  hamburger.addEventListener('click', function () {
+    menu.classList.toggle('active');
+  });
+});
